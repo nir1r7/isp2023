2efc4f25d3a55250cb2f8291d4f22545ddfb3f8b
Cqctxs <seanzhao2007@gmail.com>
2023-06-05 13:41:39 -0400

    and

--- Level3.java
+++ Level3.java
@@ -5,24 +5,21 @@ public class Level3 {
     int score = 0;
     int health = 3;
     Player p = new Player(750, 350);
-    Obstacle o1 = new Obstacle(200, 200, 50, 50, true);
-    FallingObstacle o2 = new FallingObstacle(600, 200, 50, 50, false);
+    Obstacle[] obstacles = {
+        new Obstacle(200, 200, 50, 50, true),
+        new FallingObstacle(600, 200, 50, 50, false)
+    };
 
     public void level3(Graphics g) {
         g.drawString("Score: " + score, 1000, 200);
         g.drawString("Health: " + health, 1000, 300);
         p.display(g);
-        o1.display(g);
-        o2.display(g);
-        int collider1 = p.collided(o1);
-        int collider2 = p.collided(o2);
-        if (collider1 != 0) {
-            if (collider1 > 0) score++;
-            else health--;
-        }
-        if (collider2 != 0) {
-            if (collider2 > 0) score++;
-            else health--;
+        for (Obstacle o : obstacles) {
+            o.display(g);
+            if (p.collided(o) != 0) {
+                if (p.collided(o) > 0) score++;
+                else health--;
+            }
         }
     }
 
