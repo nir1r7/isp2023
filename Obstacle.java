8186f8932b75a30ba351d37a2ab777b4ac38d7f2
Nirvan Rabbani <nir1r7@gmail.com>
2023-06-09 00:25:27 -0400

    added buttons ;

--- Obstacle.java
+++ Obstacle.java
@@ -14,25 +14,6 @@ public class Obstacle{
     boolean good;
     BufferedImage img;
 
-    Button y0;
-    Button n0;
-    Button y1;
-    Button n1;
-    Button y2;
-    Button n2;
-
-    Button y3;
-    Button n3;
-
-    Button y4;
-    Button n4;
-    Button y5;
-    Button n5;
-    Button y6;
-    Button n6;
-    
-    Button y7;
-    Button n7;
 
     public Obstacle(double x, double y, int w, int h, boolean good, BufferedImage img) {
         this.x = x;
@@ -94,76 +75,5 @@ public class Obstacle{
 
     public void setY(double n) {
         y = n;
-    }
-
-    public void message(Graphics g, int i){
-        switch(i){
-            case 0:
-                Slide s1 = new Slide(1, true,  "Testing 0");
-                s1.display(g);
-
-                y0 = new Button("Yes 0", 950, 200, 300, 50);
-                n0 = new Button("No 0", 950, 350, 300, 50);
-
-                y0.display(g);
-                n0.display(g);
-
-                break;
-            case 1:
-                Slide s2 = new Slide(1, true,  "Testing 1");
-                s2.display(g);
-
-                y1 = new Button("Yes 1", 950, 200, 300, 50);
-                n1 = new Button("No 1", 950, 350, 300, 50);
-
-                y1.display(g);
-                n1.display(g);
-
-                break;
-            case 2:
-                Slide s3 = new Slide(1, true,  "Testing 2");
-                s3.display(g);
-
-                y2 = new Button("Yes 2", 950, 200, 300, 50);
-                n2 = new Button("No 2", 950, 350, 300, 50);
-
-                y2.display(g);
-                n2.display(g);
-                
-                break;
-            case 3:
-                Slide s4 = new Slide(-1, true,  "Testing 3");
-                s4.display(g);
-
-                y3 = new Button("Yes 3", 950, 200, 300, 50);
-                n3 = new Button("No 3", 950, 350, 300, 50);
-
-                y3.display(g);
-                n3.display(g);
-                
-                break;
-            case 4:
-                Slide s5 = new Slide(-1, true,  "Testing 4");
-                s5.display(g);
-
-                y4 = new Button("Yes 4", 950, 200, 300, 50);
-                n4 = new Button("No 4", 950, 350, 300, 50);
-
-                y4.display(g);
-                n4.display(g);
-                
-                break;
-            case 5:
-                Slide s6 = new Slide(-1, true,  "Testing 5");
-                s6.display(g);
-
-                y5 = new Button("Yes 5", 950, 200, 300, 50);
-                n5 = new Button("No 5", 950, 350, 300, 50);
-
-                y5.display(g);
-                n5.display(g);
-                
-                break;
-        }
     }
 }
