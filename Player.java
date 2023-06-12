284c35bd4a979b8d14b1d780577fa85cccddbf7d
Cqctxs <seanzhao2007@gmail.com>
2023-06-12 19:28:39 -0400

    Commented both player classes (i forgor)

--- Player.java
+++ Player.java
@@ -7,25 +7,38 @@ import java.awt.image.BufferedImage;
  * @author Nirvan Rabbani, Sean Zhao
  */
 public class Player {
-    
+
     double x;
     double y;
     int w = 20;
     int h = 20;
+
+    /** Background image of the player */
     BufferedImage background = null;
 
+    /** Boolean variables which control player movement */
     boolean left;
     boolean right;
     boolean up;
     boolean down;
 
-    boolean moving = true;
-
+    /**
+     * Player constructor
+     * @param x x value
+     * @param y y value
+     */
     public Player(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Player constructor with width and height
+     * @param x x value
+     * @param y y value
+     * @param w width
+     * @param h height
+     */
     public Player(double x, double y, int w, int h) {
         this.x = x;
         this.y = y;
@@ -33,25 +46,42 @@ public class Player {
         this.h = h;
     }
     
+    /**
+     * Player constructor with background
+     * @param x x value
+     * @param y y value
+     * @param background background image
+     */
     public Player(double x, double y, BufferedImage background) {
         this.x = x;
         this.y = y;
         this.background = background;
     }
 
+    /**
+     * Displays the player
+     * @param g graphics
+     */
     public void display(Graphics g) {
+        /** Draws the background depending if the player has a background */
         if (background != null) {
             g.drawImage(background, (int)x, (int)y, w, h, null);
         } else {
             g.fillRect((int)x, (int)y, w, h);
         }
 
+        /** Updates the player position based off of booleans */
         if (left) updateX(-1);
         if (right) updateX(1);
         if (up) updateY(-1);
         if (down) updateY(1);
     }
 
+    /**
+     * Checks if the player is colliding with an obstacle
+     * @param o the obstacle
+     * @return 1: collided and the obstacle gives points, 2: collided and the obstalce removes points, 0: no collision
+     */
     public int collided(Obstacle o){
         if (x + w >= o.getX() && x <= o.getX() + o.getW() && y + h >= o.getY() && y <= o.getY() + o.getH()){
             if (o.good) {
@@ -63,22 +93,38 @@ public class Player {
         return 0;
     }
 
+    /**
+     * Adds a certain value to the current x value
+     * @param dx the change in x
+     */
     public void updateX(double dx) {
-        if (this.x + w + dx <= 1385 && this.x + dx >= 0 && moving) {
+        if (this.x + w + dx <= 1385 && this.x + dx >= 0) {
             this.x += dx;
         }
     }
 
+    /**
+     * Adds a certain value to the current y value
+     * @param dy the change in y
+     */
     public void updateY(double dy) {
-        if (this.y + h + dy <= 665 && this.y + dy >= 0 && moving) {
+        if (this.y + h + dy <= 665 && this.y + dy >= 0) {
             this.y += dy;
         }
     }
 
+    /**
+     * Sets the x value to a certain value
+     * @param n x value
+     */
     public void setX(int n){
         x = n;
     }
 
+    /**
+     * Sets the y value to a certain value
+     * @param n y value
+     */
     public void setY(int n){
         y = n;
     }
--- Player2.java
+++ Player2.java
@@ -5,21 +5,45 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 /**
- * Player class for Finals Frenzy, creates a user-controlled player for the game
+ * Player class for Finals Frenzy, creates a user-controlled player for specifically level 2
  * Course: ICS4U0-1
  * @author Nirvan Rabbani, Sean Zhao
  */
 public class Player2 extends Player {
 
+    /** Sprites for each of the walking directions */
     BufferedImage frontSprite;
     BufferedImage backSprite;
     BufferedImage leftSprite;
     BufferedImage rightSprite;
 
+    /** Moving variable to restrict movement in slides */
+    boolean moving = true;
+
+    /**
+     * Player2 Constructor
+     * @param x x coordinate
+     * @param y y coordinate
+     */
     public Player2(double x, double y) {
         super(x, y);
+        try {
+            frontSprite = ImageIO.read(new File("./static/img/front.png"));
+            backSprite = ImageIO.read(new File("./static/img/back.png"));
+            leftSprite = ImageIO.read(new File("./static/img/left.png"));
+            rightSprite = ImageIO.read(new File("./static/img/right.png"));
+
+            background = frontSprite;
+        } catch (Exception e) {}
     }
 
+    /**
+     * Player2 Constructor with width and height
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param w width
+     * @param h height
+     */
     public Player2(double x, double y, int w, int h) {
         super(x, y, w, h);
         try {
@@ -29,22 +53,32 @@ public class Player2 extends Player {
             rightSprite = ImageIO.read(new File("./static/img/right.png"));
 
             background = frontSprite;
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
+        } catch (Exception e) {}
     }
     
+    /**
+     * Player2 Constructor with background
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param background background image
+     */
     public Player2(double x, double y, BufferedImage background) {
         super(x, y, background);
     }
 
+    /**
+     * Displays the player
+     * @param g graphics
+     */
     public void display(Graphics g) {
+        /** Draws the background depending if the player has a background */
         if (background != null) {
             g.drawImage(background, (int)x, (int)y, w, h, null);
         } else {
             g.fillRect((int)x, (int)y, w, h);
         }
 
+        /** Changes sprites based on the direction the player is moving */
         if (left) {
             updateX(-1);
             background = leftSprite;
@@ -63,6 +97,33 @@ public class Player2 extends Player {
         }
     }
 
+    /**
+     * Adds a certain value to the current x value
+     * @param dx the change in x
+     */
+    public void updateX(double dx) {
+        /** Only moves if moving is true */
+        if (this.x + w + dx <= 1385 && this.x + dx >= 0 && moving) {
+            this.x += dx;
+        }
+    }
+
+    /**
+     * Adds a certain value to the current y value
+     * @param dy the change in y
+     */
+    public void updateY(double dy) {
+        /** Only moves if moving is true */
+        if (this.y + h + dy <= 665 && this.y + dy >= 0 && moving) {
+            this.y += dy;
+        }
+    }
+
+    /**
+     * Checks if the player is collided with a wall
+     * @param wall the wall
+     * @return the direction in which the wall is collided with or no collision at all
+     */
     public int collided(Wall wall){
         if (x + w >= wall.getX() && x + w <= wall.getX() + 25 && y + h >= wall.getY() && y <= wall.getY() + wall.getH()){
             return 1;
@@ -79,10 +140,18 @@ public class Player2 extends Player {
         return 0;
     }
 
+    /**
+     * Sets moving to true or false
+     * @param m moving or not
+     */
     public void setMoving(boolean m){
         moving = m;
     }
 
+    /**
+     * Gets if moving is true or false
+     * @return boolean of moving
+     */
     public boolean isMoving(){
         return moving;
     }
